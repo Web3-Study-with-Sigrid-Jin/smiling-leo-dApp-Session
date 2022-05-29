c426a791233eb1f691022be4fc6c3c5e41165c39
sweun2 <sweun3@gmail.com>
2022-05-30 02:15:00 +0900

    2. feat: connect metamask to website

--- week_2/sweun2/front/src/App.js
+++ week_2/sweun2/front/src/App.js
@@ -6,23 +6,82 @@ import ATNToken from './utils/ATNToken.json';
 const CONTRACT_ADDRESS ="0x56f3532eDEeb1D88757E81CB7E8030b278381d7b";
 
 const App=()=>{
-
-  const checkMetamask = async () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
 
     if (!ethereum) {
-      alert("Make sure you have metamask!");
+      console.log("Make sure you have metamask!");
       return;
     } else {
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
-      alert("metamask " ,accounts[0]);
+      console.log("metamask " );
     }
-      
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+    } else {
+      console.log("No authorized account found")
+    }
 
   }
 
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      alert("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   
+  const askContractToTransfer = async () => {
+    alert("2끝")
+  }
   
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  })
+
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+  
+  const TransferUI = () => (
+    <div>
+          
+          <button onClick={askContractToTransfer} className="cta-button connect-wallet-button">
+            Transfer
+          </button>
+    </div>
+  )
+
+  
+
   return (
     <div className="App">
       <div className="container">
@@ -31,8 +90,9 @@ const App=()=>{
           <p className="sub-text">
             Long Live Smiling Leo, Long Live DSRV!
           </p>
-          {checkMetamask()}
+          {currentAccount === "" ? renderNotConnectedContainer() : TransferUI()}
         </div>
+        
       </div>
     </div>
   );
